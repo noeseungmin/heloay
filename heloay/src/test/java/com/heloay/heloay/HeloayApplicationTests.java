65ef296a9c2a59f6d3d3457006b0ce2fa062b5db
seungmin <nowinmin96@gmail.com>
2023-02-01 20:17:57 +0900

    #5 - 스프링 프로젝트 시작
    * 스프링 웹
    * 롬복
    *스프링 JPA

---
+++ heloay/src/test/java/com/heloay/heloay/HeloayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heloay.heloay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeloayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

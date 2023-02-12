692ba036a66c3848abd86e44e041c035e537c779
seungmin <nowinmin96@gmail.com>
2023-02-12 21:48:01 +0900

    #14-영화 등록 리스트 페이징 처리
    영화 등록 페이지 한 페이지에 10개 데이터 보여 줄 수 있게 페이징 처리

--- heloay/src/main/java/com/heloay/heloay/controller/MovieController.java
+++ heloay/src/main/java/com/heloay/heloay/controller/MovieController.java
@@ -1,2 +1,52 @@
-package com.heloay.heloay.controller;public class MovieController {
+package com.heloay.heloay.controller;
+
+import com.heloay.heloay.domain.Movie;
+import com.heloay.heloay.dto.MovieDto;
+import com.heloay.heloay.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+
+    @GetMapping("main/movie")
+    public String movie(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
+        Page<Movie> paging = this.movieService.movieList(page);
+        model.addAttribute("paging", paging);
+        return "view/movie";
+    }
+
+    @GetMapping("movie/detail/{movieId}")
+    public String detailMovie(@PathVariable Long movieId, Model model){
+        Movie movie = movieService.findMovie(movieId);
+        model.addAttribute("movie", movie);
+        return "view/detail";
+    }
+
+    @GetMapping("movie/new")
+    public String newMovie(Model model){
+        model.addAttribute("movieform", new Movie());
+        return "view/registration";
+    }
+
+    @PostMapping("movie/new")
+    public String movieCreate(MovieDto dto,
+                              @RequestParam("imgUrl") MultipartFile poster) throws IOException {
+        Movie movie = movieService.create(dto, poster);
+        return "redirect:/movie/detail/" + movie.getId();
+    }
 }

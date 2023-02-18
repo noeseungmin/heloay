ed45edf57368a257c104ea15dacee3c77128bf13
seungmin <nowinmin96@gmail.com>
2023-02-18 12:23:32 +0900

    #24-메인화면 변경
    메인화면에서 영화 리스트 보여줄 수 있게 뷰, 컨트롤러 구성

--- heloay/src/main/java/com/heloay/heloay/controller/MainController.java
+++ heloay/src/main/java/com/heloay/heloay/controller/MainController.java
@@ -1,18 +1,27 @@
 package com.heloay.heloay.controller;
 
 import com.heloay.heloay.domain.Movie;
+import com.heloay.heloay.service.MovieService;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
-    @GetMapping("main")
-    public String main(Model model){
-        model.addAttribute("data", new Movie());
-        return "view/main";
+    private final MovieService movieService;
 
+    @GetMapping("main")
+    public String movie(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
+        Page<Movie> paging = this.movieService.movieList(page);
+        model.addAttribute("paging", paging);
+        return "view/main";
     }
 }
